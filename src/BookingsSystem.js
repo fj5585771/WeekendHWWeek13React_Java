f896c2c4de03b99522011c1e070c2959aed1878c
fraser jamieson <fraserjamieson.93@gmail.com>
2021-02-12 16:19:03 +0000

    started react 1st component

---
+++ src/BookingsSystem.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BookingsSystem = () => {
+    return (
+
+
+        // This will control the state of the front end
+        <h3> i will be the API endpoint component! </h3>
+
+        
+
+
+    
+    );
+}
+
+export default BookingsSystem;
