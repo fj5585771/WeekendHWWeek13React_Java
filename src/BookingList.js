d074100fb7a93f6a5b8e1595ed49f1d891a9ab4f
fraser jamieson <fraserjamieson.93@gmail.com>
2021-02-12 17:02:10 +0000

    2 components added

---
+++ src/BookingList.js
@@ -0,0 +1,12 @@
+import BookingsSystem from "./BookingsSystem";
+import React from "react";
+
+const BookingList = () => {
+
+    return (
+        <h3> i will be the API endpoint component! </h3>
+    )
+
+}
+
+export default BookingList;
--- src/BookingsSystem.js
+++ src/BookingsSystem.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import BookingList from "./BookingList";
 
 const BookingsSystem = () => {
+
+    const [bookings, setBookings] = useState({});
+    const [selectedBookingId, setBookingId] = useState(1);
+
+    const getBookings = () => {
+        console.log("getting bookings information")
+
+        fetch('/bookings')
+        .then(res => res.json())
+        .then(data => setBookings(data));
+
+    }
+
+    useEffect(() => {
+        getBookings();
+    });
+
     return (
 
-
-        // This will control the state of the front end
-        <h3> i will be the API endpoint component! </h3>
-
         
+        // This will control the state of the front end
+        <>
+            <h1>Bookings information</h1>
+            <BookingList />
+        </>
 
 
     
